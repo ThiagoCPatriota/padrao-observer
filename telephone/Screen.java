3e6dc1a5690197cd2216c971d1c8671c7e020e45
Thiago Patriota <74380700+ThiagoCPatriota@users.noreply.github.com>
2026-09-21 15:50:05 -0300

    Adiciona observador do ultimo digito

--- telephone/Screen.java
+++ telephone/Screen.java
@@ -1,12 +1,18 @@
 /**
  * Prints things out to the screen, when needed
  * Printing to the screen:
- *  System.out.println("hello");
+ * System.out.println("hello");
  */
 public class Screen {
     private final PhoneModel model;
 
     public Screen(PhoneModel model) {
         this.model = model;
+        model.addObserver(new Observer() {
+            @Override
+            public void update(int newDigit) {
+                System.out.println("Latest digit: " + newDigit);
+            }
+        });
     }
 }
