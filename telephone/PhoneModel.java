17bb5c789cd83e2a5a58f83d3d49732e10d659c2
Thiago Patriota <74380700+ThiagoCPatriota@users.noreply.github.com>
2026-09-21 15:49:03 -0300

    Transforma PhoneModel em objeto observado

--- telephone/PhoneModel.java
+++ telephone/PhoneModel.java
@@ -6,9 +6,21 @@ import java.util.List;
  */
 public class PhoneModel {
     private List<Integer> digits = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
 
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        notifyObservers(newDigit);
+    }
+
+    private void notifyObservers(int newDigit) {
+        for (Observer observer : observers) {
+            observer.update(newDigit);
+        }
     }
 
     public List<Integer> getDigits() {
